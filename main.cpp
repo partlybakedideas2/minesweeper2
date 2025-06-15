338effbaceee0f6326c0bf743d9a78747cdc77d8
Alan McPherson <mcphersonalanm@gmail.com>
2025-06-15 10:06:43 -0400

    Integrated generated board with UI file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "minesweeper2.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Minesweeper2 w;
+    MainWindow w;
     w.setStyleSheet("Tile {background-color: rgb(25, 178, 255); font: bold 20px}"
                     "Tile:hover {background-color: rgb(146, 213, 247)}"
                     "Tile:disabled {background-color: rgb(209, 209, 209)}");
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,11 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this); // This sets up any elements defined in the .ui file
 
-    Board* b = new Board(EASY, this); // Create the board object on the heap
-
-    QVBoxLayout* layout = new QVBoxLayout(this);
-    layout->addWidget(b); // Add the board to the layout
-    setLayout(layout); // Set the layout for the Minesweeper2 window
+    board b = new Board(EASY, ui->boardParentWidget);
 }
 
 MainWindow::~MainWindow()
